a4de01807da7b1afc8caff0d1ee6610ee410de2a
Lorenzo Godi <34336808+LorenzoGodi@users.noreply.github.com>
2024-10-09 19:28:41 +0200

    Added download cv button

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 document.getElementById('enBtn').addEventListener('click', function() {
-    switchLanguage('en');
+    lang = 'en';
+    switchLanguage();
 });
 
 document.getElementById('itBtn').addEventListener('click', function() {
-    switchLanguage('it');
+    lang = 'it';
+    switchLanguage();
 });
 
-function switchLanguage(lang) {
+document.getElementById('download-btn').addEventListener('click', downloadLatestCV);
+
+
+function switchLanguage() {
     let elements = document.querySelectorAll('[data-lang]');
     elements.forEach(function(el) {
         if (el.getAttribute('data-lang') === lang) {
@@ -20,5 +25,34 @@ function switchLanguage(lang) {
     document.getElementById('itBtn').classList.toggle('active', lang === 'it');
 }
 
+async function downloadLatestCV() {
+    filename = lang === 'en' ? 'LorenzoGodi_CV_en.pdf' : 'LorenzoGodi_CV_it.pdf';
+
+    try {
+        // Fetch the latest release info from GitHub API
+        const response = await fetch(`https://api.github.com/repos/LorenzoGodi/cv/releases/latest`);
+        const release = await response.json();
+        // Find the asset with the desired filename
+        const asset = release.assets.find(asset => asset.name == filename);
+        
+        if (asset) {
+            // Create an anchor element to trigger the download
+            const link = document.createElement('a');
+            link.href = asset.browser_download_url;
+            link.download = filename;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        } else {
+            alert('File not found in the latest release.');
+        }
+    } catch (error) {
+        console.error('Error fetching the latest release:', error);
+        alert('Failed to download the file.');
+    }
+}
+
+
 // Initial setup: Display English by default
-switchLanguage('en');
+lang = 'en';
+switchLanguage();
